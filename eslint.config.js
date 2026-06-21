890f47afc4bae5df5eb031ef41c8d407fffdff35
FernandoVerticalize <fernandoverticalize@gmail.com>
2026-06-21 10:12:57 -0300

    chore(deps): downgrade eslint to version 9.21.0
    Downgraded eslint and @eslint/js to version 9.21.0 to ensure compatibility with the current environment and added dev-dist to the ignore patterns.

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "dev-dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
